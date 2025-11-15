4820fcf51e1e3b1ffa66ca5bbd9071f936723406
shuly69 <shulenin69@gmail.com>
2025-11-15 19:01:27 +0000

    Add superuser creation step to build script

--- newsproject/build.sh
+++ newsproject/build.sh
@@ -6,8 +6,16 @@ echo "Applying migrations..."
 python manage.py migrate --noinput
 
 
-
-
+echo "Creating superuser..."
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
 
 
 echo "Starting server..."
