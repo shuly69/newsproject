35cd642a734d07452cc0c3fd64f4d89cdee463f9
shuly69 <shulenin69@gmail.com>
2025-11-17 21:07:06 +0000

    user model fix forms

--- newsproject/user/forms.py
+++ newsproject/user/forms.py
@@ -7,14 +7,14 @@ from django.contrib.auth.forms import UserCreationForm
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-input', 'placeholder': 'Enter your email', 'id': 'email'}))
     bio = forms.CharField(required=False, widget=forms.Textarea(attrs={'class': 'form-textarea', 'placeholder': 'Tell us about yourself', 'rows': 4}))
-    username = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Choose a username', 'id': 'username'}))
+    username = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Choose a username', 'id': 'username'}))
     first_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'First Name', 'id': 'firstName'}))
     last_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Last Name', 'id': 'lastName'}))
     password1 = forms.CharField(required=True, label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input', 'placeholder': 'Enter your password', 'id': 'password'}))
     password2 = forms.CharField(required=True, label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-input', 'placeholder': 'Confirm your password', 'id': 'confirmPassword'}))
     class Meta:
         model = CustomUser
-        fields = ['*']
+        fields = ['username', 'email', 'bio', 'specialization', 'first_name', 'last_name', 'password1', 'password2']
         widgets = {
             'specialization': forms.Select(attrs={'class': 'form-select', 'id': 'specialization'}),
         }
